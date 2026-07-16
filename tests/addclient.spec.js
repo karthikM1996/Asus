3363987cec6048043856f8be9f463d4d0e3f3124
sbabludeva <immucorneo@gmail.com>
2026-07-16 14:31:42 +0530

    changed

--- tests/addclient.spec.js
+++ tests/addclient.spec.js
@@ -1,9 +1,5 @@
 import {test} from '@playwright/test'
 test('add client', async ({page}) => {
-<<<<<<<<< Temporary merge branch 1
   await page.goto('http://localhost:3000/clients');})
-=========
-  await page.goto('http://localhost:3000/clients')
-})
-  
->>>>>>>>> Temporary merge branch 2
+
+
