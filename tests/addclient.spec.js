55930355ec4d3d5067011ba9709f345a6b27cee1
sbabludeva <immucorneo@gmail.com>
2026-07-16 13:28:02 +0530

    modified

--- tests/addclient.spec.js
+++ tests/addclient.spec.js
@@ -1,7 +1,3 @@
 import {test} from '@playwright/test'
 test('add client', async ({page}) => {
-  await page.goto('http://localhost:3000/clients')
-  await page.getByRole('link', {name: 'Add Client'}).click()
-  await page.getByLabel('Name').fill('Test Client')
-  await page.getByLabel('Email').fill('')})
-  
+  await page.goto('http://localhost:3000/clients');})
