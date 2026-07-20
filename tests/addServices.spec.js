14f6d0955747c12ca40860413e44f595ec072cc6
Vinusha6244 <Vinusha6244@gmail.com>
2026-07-20 13:04:29 +0530

    modified

--- tests/addServices.spec.js
+++ tests/addServices.spec.js
@@ -3,8 +3,8 @@ import {test,expect} from "../customeFixture/pagesFixture"
 
 test("addservices",async({page,loginPage,homePage,servicePage})=>
 {
-    await  page.goto("http://49.249.29.4:8081/TestServer/Build/Client_Management_System/admin/index.php")
-    await loginPage.navigate()
+    //await page.goto("http://49.249.29.4:8081/TestServer/Build/Client_Management_System/admin/dashboard.php")
+    await page.goto(process.env.HOMEPAGE_URL)
     await homePage.servicesLink.click()
     await servicePage.clickAddServices()
     let serviceName="ComputerService"
