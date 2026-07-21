7f909e2d58a54cacb17d3caf440468730a62dba8
karthik <mahadevakarthik@gmail.com>
2026-07-21 14:55:24 +0530

    add jenkinslfie

--- pages/HomePage.page.js
+++
@@ -1,9 +0,0 @@
-export class HomePage
-{
-    constructor(page)
-    {
-        this.page=page
-        this.servicesLink=page.locator(" //span[text()=' Services']")
-        
-    }
-}
--- tests/suni.spec.js
+++
@@ -1,8 +0,0 @@
-import test from "playwright/test";
-
-test("test",async({pages})=>
-{
-console.log(`sunifiles`)
-
-    console.log("hiiii")
-})
